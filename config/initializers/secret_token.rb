98402d86f65c5859094a96cd589d2c677aca4bd0
heronmedeiros <heron.medeiros@gmail.com>
2010-10-18 09:54:17 -0300

    criando projeto

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+GalleryDevise::Application.config.secret_token = 'fad1e34b2ecfc1430aa7e6a038ae3ef8c70d5c4508f42aaa2ae8c3ea05be74ca7d9b37503cf0fa429f08931c8c76f3dcef778a0a831c3dd7f97eb424ba0465da'
